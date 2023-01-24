93c687761b98f9f4265b2bf996cd888acf87c8a8
weldone-dev <roma.kalita2015@gmail.com>
2023-01-24 10:12:33 +0300

    Refact

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import Logo from "./components/Logo.js";
 (function () {
     const pjs = new PointJS(680, 480, {})
     const game = pjs.game
-
     const logo = new Logo(game)
 
     game.newLoop("game", () => {
         logo.render()
     })
+
     game.setLoop("game")
     game.start()
 })()
